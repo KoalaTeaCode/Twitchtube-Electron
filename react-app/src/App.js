52acf577b080a0ae6cc4470de2a6495435b8990a
thehollidayinn <krh121791@gmail.com>
2017-08-31 15:14:56 -0600

    Added router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,22 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+
 import Nav from './nav/Nav';
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const Commands = () => (
+  <div>
+    <h2>Commands</h2>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App container">
-        <div className="row">
-          <section className='col-4'>
-            <Nav value={'test'} />
-          </section>
+      <Router>
+        <div className="App container">
+          <div className="row">
+            <section className='col-2'>
+              <Nav value={'test'} />
+            </section>
 
-          <section className="col-8 App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </section>
+            <section className="col-10 App-intro">
+              <Route exact path="/" component={Home} />
+              <Route exact path="/commands" component={Commands} />
+            </section>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- react-app/src/nav/Nav.js
+++ react-app/src/nav/Nav.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import {
+  Link,
+} from 'react-router-dom'
 
+// Electron
 const electron = window.require('electron');
 const fs = electron.remote.require('fs');
 const ipcRenderer  = electron.ipcRenderer;
 
+// <h2 onClick={this.handleClick}> Testing {this.props.value} </h2>
 class Nav extends Component {
   handleClick () {
     ipcRenderer.send('asynchronous-message', 'ping')
@@ -11,7 +16,10 @@ class Nav extends Component {
 
   render() {
     return (
-      <h2 onClick={this.handleClick}> Testing {this.props.value} </h2>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/commands">Commands</Link></li>
+      </ul>
     );
   }
 }
